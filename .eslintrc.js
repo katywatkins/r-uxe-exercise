421ac3946832f77e9e27e9bd87d667a4ded61628
Katy Watkins <watkins.katy@gmail.com>
2022-11-22 07:56:06 -0500

    add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,11 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: ['airbnb', 'plugin:storybook/recommended'],
   rules: {
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
-      },
-    ],
-    'react/jsx-props-no-spreading': [
-      0,
-    ],
+    'react/function-component-definition': [2, {
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'arrow-function',
+    }],
+    'react/jsx-props-no-spreading': [0],
   },
   env: {
     browser: true,
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Regions UXE Exercise
+
+Prerequisites:
+Node 16 (This was build with 16.13.0)
+
+## notes to self:
+If need npm7 storybook migration, run `npx storybook@next automigrate`
